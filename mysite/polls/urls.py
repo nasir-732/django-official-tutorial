5fc2ea10acc7841f992b84cc72a12d678408d0cf
Nasir Noor <nasirnoor732@gmail.com>
2023-07-20 09:37:31 +0500

    inlude added

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+
 from .import views
 app_name ="polls"
 urlpatterns=[
@@ -8,5 +9,11 @@ urlpatterns=[
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
-   ]
+    path("<int:question_id>/votes/", views.vote, name="vote"),
+
+    path("<int:question_id>/newchoice/", views.newchoice, name="newchoice"),
+
+    path("<int:question_id>/newchoice/", views.newchoice, name="newchoice"),
+]
+    
+
